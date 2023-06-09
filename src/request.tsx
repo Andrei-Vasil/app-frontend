a01d7124e2b55ec2d70d635de91b44c6bf625a19
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-06-09 16:04:30 +0300

    modified GET request to POST

--- src/request.tsx
+++ src/request.tsx
@@ -1,7 +1,7 @@
 export async function sendRequest(language: string, no_of_requests: number, request: string | undefined, request_size: number | undefined): Promise<Response | undefined> {
     if (request !== undefined) {
         return await fetch(`http://localhost:5000/custom/${language}/${no_of_requests}`, {
-            method: 'GET',
+            method: 'POST',
             body: JSON.stringify({
                 'request': request
             }),
