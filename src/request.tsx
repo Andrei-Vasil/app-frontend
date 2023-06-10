a5af88a5e8acee432ad84f7d238831aa9af02351
Andrei Vasil <andrei.vasil.271@gmail.com>
2023-06-10 12:37:55 +0300

    done

--- src/request.tsx
+++ src/request.tsx
@@ -1,6 +1,6 @@
 export async function sendRequest(language: string, no_of_requests: number, request: string | undefined, request_size: number | undefined): Promise<Response | undefined> {
     if (request !== undefined) {
-        return await fetch(`http://localhost:5000/custom/${language}/${no_of_requests}`, {
+        return await fetch(`http://localhost:24511/custom/${language}/${no_of_requests}`, {
             method: 'POST',
             body: JSON.stringify({
                 'request': request
@@ -11,7 +11,7 @@ export async function sendRequest(language: string, no_of_requests: number, requ
         })
     }
     if (request_size !== undefined) {
-        return await fetch(`http://localhost:5000/random/${language}/${no_of_requests}/${request_size}`, {    
+        return await fetch(`http://localhost:24511/random/${language}/${no_of_requests}/${request_size}`, {    
             method: 'GET',
             headers: {
                 'Content-type': 'application/json; charset=UTF-8'
